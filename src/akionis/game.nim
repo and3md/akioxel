9262dbcef77739db7c02d7900f84144dbb3175b9
and3md <and3md@gmail.com>
2026-04-07 16:36:42 +0200

    First  type: Game draft

---
+++ src/akionis/game.nim
@@ -0,0 +1,33 @@
+# This is just an example to get you started. Users of your library will
+# import this file by writing ``import akioxel/submodule``. Feel free to rename or
+# remove this file altogether. You may create additional modules alongside
+# this file as required.
+
+from raylib as ray import nil
+
+type Game* = ref object of RootObj
+  title: string
+
+var instance: Game
+
+proc getGame*(): Game  =
+  if instance.isNil:
+    raise newException(ValueError, "No game instance")
+  return instance
+
+proc initGame*(windowWidth, windowHeight: int32, title: string) =
+  ## Initialises a new ``Game`` object.
+  if instance.isNil:
+    instance = Game(title: title)
+    ray.initWindow(windowWidth, windowHeight, title)
+  else:
+    raise newException(ValueError, "Game already created")
+
+proc run*(game: Game) =
+  while not ray.windowShouldClose():
+    ray.beginDrawing()
+    ray.clearBackground(ray.DarkGray)
+    ray.endDrawing()
+
+proc title(game: Game): string =
+  return game.title
