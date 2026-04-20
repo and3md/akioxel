b33912ceb460e399527ce515be9218b28cb5f5a8
and3md <and3md@gmail.com>
2026-04-20 14:55:36 +0200

    Clean up akinis.nim file

--- src/akionis.nim
+++ src/akionis.nim
@@ -1,15 +1,10 @@
-# This is just an example to get you started. A typical library package
-# exports the main API in this file. Note that you cannot rename this file
-# but you can remove it if you wish.
+# Exported the main API from other engine files
+
 import akionis/base_types
 import akionis/game_loop
 import akionis/matrices
 import akionis/colors
 import akionis/components/square
-
-proc add*(x, y: int): int =
-  ## Adds two numbers together.
-  return x + y
 
 export 
   matrices, colors, base_types, game_loop, square
