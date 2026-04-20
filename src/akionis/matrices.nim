875965f7de0bdff2ebb020c84cb96791c7b72967
and3md <and3md@gmail.com>
2026-04-20 15:39:02 +0200

    Add Vector3 type and calculate global bounding rect from 4 points and world matrix

--- src/akionis/matrices.nim
+++ src/akionis/matrices.nim
@@ -3,7 +3,9 @@ import vmath
 from raylib as ray import nil
 
 type
-  Matrix3* = GMat3[float32]    
+  Matrix3* = GMat3[float32]
+  Vector3* = GVec3[float32]
+
   Rect* = ray.Rectangle
 
 proc decomposeMatrix*(
@@ -15,3 +17,19 @@ proc decomposeMatrix*(
   result.angle = math.arctan2(matrix[0, 1], matrix[0, 0])
   result.scaleX = vec2(matrix[0, 0], matrix[0, 1]).length
   result.scaleY = vec2(matrix[1, 0], matrix[1, 1]).length
+
+proc globalRectFromVec3(worldMatrix: Matrix3, p1, p2, p3, p4: Vector3): Rect =
+  let world_p1 = worldMatrix * p1
+  let world_p2 = worldMatrix * p2
+  let world_p3 = worldMatrix * p3
+  let world_p4 = worldMatrix * p4
+
+  let min_x = min([world_p1.x, world_p2.x, world_p3.x, world_p4.x])
+  let max_x = max([world_p1.x, world_p2.x, world_p3.x, world_p4.x])
+  let min_y = min([world_p1.y, world_p2.y, world_p3.y, world_p4.y])
+  let max_y = max([world_p1.y, world_p2.y, world_p3.y, world_p4.y])
+  result.x = min_x
+  result.y = min_y
+  result.width = max_x - min_x
+  result.height = max_y - min_y
+  
