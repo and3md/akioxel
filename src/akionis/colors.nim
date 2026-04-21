f31d1759c99fc4d0462d72ffed27b67e1a9061bc
and3md <and3md@gmail.com>
2026-04-21 10:51:35 +0200

    Add magenta color const

--- src/akionis/colors.nim
+++ src/akionis/colors.nim
@@ -7,4 +7,4 @@ const Blue* = Color(r: 0, g: 0, b: 255, a: 255)
 const White* = Color(r: 255, g: 255, b: 255, a: 255)
 const Black* = Color(r: 0, g: 0, b: 0, a: 255)
 const Yellow* = Color(r: 255, g: 255, b: 0, a: 255)
-
+const Magenta* = Color(r: 255, g: 0, b: 255, a: 255)
