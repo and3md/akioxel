07a3f9d3eb807e13f2c10f98b132f0ab8587231d
and3md <and3md@gmail.com>
2026-05-01 23:32:09 +0200

    More Scrollbar getters and setters

--- src/akionis/components/ui/scroll_bar.nim
+++ src/akionis/components/ui/scroll_bar.nim
@@ -13,8 +13,17 @@ type
     orientation: Orientation
     maxValue: int32 = 100
     value: int32 = 0
-    valueChanged: proc(newValue: int32)
+    onValueChanged: proc(newValue: int32)
     minThumbSize: int32 = 10
+
+const scrollBarThicknes = 10
+
+proc `orientation=`*(comp: ScrollBar, newOrientation: Orientation)
+
+proc newScrollBar*(orientation: Orientation, name: string): ScrollBar =
+  result = new(ScrollBar)
+  initUiComponent(result, name)
+  `orientation=`(result, orientation)
 
 proc orientation*(comp: ScrollBar): Orientation =
   return comp.orientation
@@ -23,5 +32,31 @@ proc `orientation=`*(comp: ScrollBar, newOrientation: Orientation) =
   if comp.orientation == newOrientation:
     return
   comp.orientation = newOrientation
+  case comp.orientation
+  of Orientation.Horizontal:
+    comp.widthFactor = 1
+    comp.heightFactor = 0
+  of Orientation.Vertical:
+    comp.widthFactor = 0
+    comp.heightFactor = 1
   comp.uiNeedsMinSizeUpdate
+
+proc maxValue*(comp: ScrollBar): int32 =
+  return comp.maxValue
+
+proc `maxValue=`*(comp: ScrollBar, newMaxValue: int32) =
+  if comp.maxValue == newMaxValue:
+    return
+  comp.maxValue = newMaxValue
+  # TODO: should be value changed
+
+proc value*(comp: ScrollBar): int32 =
+  return comp.value
+
+proc `value=`*(comp: ScrollBar, newValue: int32) =
+  if comp.value == newValue:
+    return
+  comp.value = newValue
+  if not comp.onValueChanged.isNil:
+    comp.onValueChanged(newValue)
 
