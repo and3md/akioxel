c22efbea401631b5e66152c37a2b527a619bd9d5
and3md <and3md@gmail.com>
2026-05-05 15:27:11 +0200

    Code formatting

--- src/akionis/components/ui/content_offset_view.nim
+++ src/akionis/components/ui/content_offset_view.nim
@@ -4,28 +4,29 @@ import ../../matrices
 import math
 from raylib as ray import nil
 
-type
-  ContentOffsetView* = ref object of UiComponent
-    ## A component that clips the child's drawing to ContentOffsetView size with clipping
-    ## Only first child with UiComponent is taken into account
-    contentOffsetX: int32
-    contentOffsetY: int32
-    contentMinSize: Size ## Content minimum size computed in calculateMinSize()
-    contentSize: Size ## Content size computed in updateLayout
-  
-method calculateMinSize*(comp: ContentOffsetView) =  
-  comp.minSize = Size(width: 50 + comp.padding.left + comp.padding.right, height: 50 + comp.padding.top + comp.padding.bottom)
+type ContentOffsetView* = ref object of UiComponent
+  ## A component that clips the child's drawing to ContentOffsetView size with clipping
+  ## Only first child with UiComponent is taken into account
+  contentOffsetX: int32
+  contentOffsetY: int32
+  contentMinSize: Size ## Content minimum size computed in calculateMinSize()
+  contentSize: Size ## Content size computed in updateLayout
 
 proc newContentOffsetView*(name: string): ContentOffsetView =
   result = new(ContentOffsetView)
   result.initUiComponent(name)
 
+method calculateMinSize*(comp: ContentOffsetView) =
+  comp.minSize = Size(
+    width: 50 + comp.padding.left + comp.padding.right,
+    height: 50 + comp.padding.top + comp.padding.bottom,
+  )
   applyMinMaxConstraint(comp.minSize, comp.minConstraint, comp.maxConstraint)
-  
+
   # calculate minimum child minimum size
   if comp.parent.isNil:
     return
-  
+
   let child = comp.parent.getFirstChildWithUiComponent()
   if child.isSome:
     let (childNode, childComp) = child.get()
