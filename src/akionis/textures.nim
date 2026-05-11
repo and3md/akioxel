848e9aea8eb39757817126fe6bef401546fa5f56
and3md <and3md@gmail.com>
2026-05-11 13:37:19 +0200

    Textures file formatting

--- src/akionis/textures.nim
+++ src/akionis/textures.nim
@@ -3,28 +3,25 @@ import colors
 import matrices
 import utils
 
-type 
+type
   Texture* = ray.Texture
 
   SharedTexture* = ref object of RootObj
     texture: Texture
 
-proc newSharedTexture*(fileType: string; fileData: openArray[uint8]): SharedTexture = 
+proc newSharedTexture*(fileType: string, fileData: openArray[uint8]): SharedTexture =
   result = new(SharedTexture)
-  var image = ray.loadImageFromMemory(fileType,fileData)
+  var image = ray.loadImageFromMemory(fileType, fileData)
   result.texture = ray.loadTextureFromImage(image)
 
-
-proc newSharedTexture*(fileType: string; fileData: string): SharedTexture = 
+proc newSharedTexture*(fileType: string, fileData: string): SharedTexture =
   return newSharedTexture(fileType, fileData.toByteSeq)
 
-
-proc width*(tex: SharedTexture): int32 = 
+proc width*(tex: SharedTexture): int32 =
   return tex.texture.width
 
 proc height*(tex: SharedTexture): int32 =
   return tex.texture.height
 
 proc drawSharedTexture*(tex: SharedTexture, x, y: float32, tint: Color = White) =
-  ray.drawTexture(tex.texture, Vector2(x: x, y:y), tint)
-
+  ray.drawTexture(tex.texture, Vector2(x: x, y: y), tint)
