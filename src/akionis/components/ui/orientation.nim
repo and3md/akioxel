7907a32391779baa3fafc6ce073b6a465f9ea104
and3md <and3md@gmail.com>
2026-05-12 19:37:17 +0200

    Add proc to get vector field for orientation

--- src/akionis/components/ui/orientation.nim
+++ src/akionis/components/ui/orientation.nim
@@ -1,3 +1,12 @@
+import ../../matrices
+
 type Orientation* {.pure.} = enum
   Horizontal
   Vertical
+
+proc getValueForOrientation*(vec: Vector2, orientation: Orientation): float32 =
+  case orientation
+  of Orientation.Horizontal:
+    return vec.x
+  of Orientation.Vertical:
+    return vec.y
